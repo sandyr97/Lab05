9ef234dd6f0849f0805a7807338494b0a0e2f435
Sandy Rech <rechs97@gmail.com>
2019-03-31 22:06:46 -0500

    added working input text field for username and post and checks if empty

--- EX_3/createPosts.php
+++ EX_3/createPosts.php
@@ -1,34 +1,25 @@
 <?php
-
 $user= $_POST["u_name"];
+$myPost=$_POST["post1"];
+/*$mysqli = new mysqli("mysql.eecs.ku.edu", "s214r501", "Caireik7", "s214r501");
 
-$mysqli = new mysqli("mysql.eecs.ku.edu", "s214r501", "Caireik7", "s214r501");
-
-/* check connection */
+/* check connection
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-
-  if (empty($user))
+*/
+  if (empty($user) || empty($myPost))
   {
-    echo "No username entered. Please, try again. <br>";
+    echo "Username or Post not entered. Please, try again. <br>";
   }
   else {
     echo "Username: ".$user. "<br>";
-    $query="INSERT INTO Users(user_id) VALUES ('$users');";
-    if ($result = $mysqli->query($query)) {
-    echo "New Record created successfully";
-    /* free result set */
-    $result->free();
+    echo "Post: ".$myPost. "<br>";
     }
-    else{
-      echo "Error: " . $mysqli . "<br>" . $mysqli->error;
-    }
-  }
 
   /* close connection */
-$mysqli->close();
+#$mysqli->close();
 
  ?>
