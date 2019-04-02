aa8ea1c906475dce38d471a9c8a3ddcc41fc9d88
Sandy Rech <rechs97@gmail.com>
2019-04-02 10:20:53 -0600

    added table of users

--- EX_5/viewUsers.php
+++ EX_5/viewUsers.php
@@ -1,17 +1,29 @@
 <?php
-echo "<table>";
-for ($i=1; $i <=100 ; $i++) {
-  echo ('<tr>');
-  for ($j=1; $j <=100 ; $j++) {
-    echo ('<td>'. $i*$j. '</td>');
-
-
-    #echo (string)($i*$j). " ";
-  }
-
-  '</tr>';
+$mysqli = new mysqli("mysql.eecs.ku.edu", "s214r501", "Caireik7", "s214r501");
+/* check connection */
+if ($mysqli->connect_error) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
 }
-'</table>';
-
-
+$query = "SELECT user_id FROM Users;";
+$result = $mysqli->query($query);
+if ($result->num_rows > 0) {
+  // output data to user
+  echo "<table>
+  <tr>
+  <th>User IDs</th>
+  </tr>";
+  while($row = $result->fetch_assoc()) {
+    $user = $row["user_id"];
+    echo "<tr>";
+    echo "<td>" . $user . "</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+}
+else {
+  echo "There are currently no existing Users";
+}
+/* close connection */
+$mysqli->close();
 ?>
