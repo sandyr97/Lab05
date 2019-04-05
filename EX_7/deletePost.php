2385498b5a4a5503ec3e56e3278e3e298c11271a
Sandy Rech <rechs97@gmail.com>
2019-04-05 12:24:21 -0500

    added backend for lab 7 that deletes

--- EX_7/deletePost.php
+++ EX_7/deletePost.php
@@ -1,5 +1,45 @@
 <?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "s214r501", "Caireik7", "s214r501");
+if ($mysqli->connect_error) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
 
+$query = "SELECT * FROM Posts;";
+$result = $mysqli->query($query);
 
+if ($stmt = mysqli_prepare($mysqli, $query)) {
+      mysqli_stmt_execute($stmt);
+      mysqli_stmt_store_result($stmt);
+      for ($i=0; $i < $number; $i++){
+        $deleteBox = $_POST['checkbox'][$i];
+        $sql = "DELETE FROM Posts WHERE post_id = '$deleteBox'";
+        $result = $mysqli->query($sql);
+      }
+      mysqli_stmt_close($stmt);
+    }
+
+  $numDeleted = count($_POST["checkbox"]);
+     echo "<h1>You deleted ". $numDeleted . " post(s)</h1>";
+     echo "<table>";
+     echo "<tr>";
+     echo "<th>Post ID</th>";
+     echo "<th>User</th>";
+     echo "<th>Post</th>";
+     echo "</tr>";
+     $query2 = "SELECT * FROM Posts;";
+     $result2 = $mysqli->query($query2);
+     while ($row = $result2->fetch_assoc()) {
+       $user = $row["author_id"];
+       $p_id = $row["post_id"];
+       $content = $row["content"];
+       echo "<tr>";
+       echo "<td>" . $p_id . "</td>";
+       echo "<td>" .$user. "</td>";
+       echo "<td>" . $content . "</td>";
+       echo "</tr>";
+     }
+     echo "</table>";
+   $mysqli->close();
 
  ?>
